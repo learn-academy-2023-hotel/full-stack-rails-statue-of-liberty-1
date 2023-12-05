1ae6ed1bf69869850ddcad72e12e82c1401e91b7
zeniavarela <zvarela8@gmail.com>
2023-12-04 16:30:05 -0800

    story 1-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  get '/blogs' => 'blog#index', as: 'blogs'
+  get '/blog/new' => 'blog#new', as: 'new_blog'
+  get '/blogs/:id' => 'blog#show', as: 'blog'
+  post '/blogs' => 'blog#create'
+  get '/blogs/:id/edit' => 'blog#edit', as: 'edit_blog'
+  patch '/blogs/:id' => 'blog#update'
+  delete '/blogs/:id' => 'blog#destroy', as: 'delete_blog'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog#index"
 end
